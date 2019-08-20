687c5006863ead9bee07dc3d76245a173c21528e
tritus <tristanferre@yahoo.fr>
2019-08-20 23:08:01 +0200

    project 4 done

--- Unit4Project4/netflix/em.py
+++ Unit4Project4/netflix/em.py
@@ -52,27 +52,29 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    shaped_X = X.reshape((X.shape[0],1,X.shape[1])).repeat(post.shape[1],axis=1)
-    shaped_post = post.reshape((post.shape[0],post.shape[1],1))
+    n, d = X.shape
+    K = post.shape[1]
 
-    ponderated_points = shaped_X*shaped_post
-    full_sum = ponderated_points.sum(axis=0)
-    weights_sum = shaped_post.sum(axis=0)
+    nKd_X = X.reshape((n,1,d)).repeat(K,axis=1)
+    nKd_post = post.reshape((n,K,1)).repeat(d, axis=2)
 
-    mu = full_sum / weights_sum
+    full_sum = np.where(nKd_X == 0, nKd_X, nKd_X*nKd_post).sum(axis=0)
+    weights_sum = np.where(nKd_X == 0, nKd_X, nKd_post).sum(axis=0)
 
-    shaped_mu = mu.reshape((1,mu.shape[0],mu.shape[1])).repeat(X.shape[0],axis=0)
-    diffs = shaped_X - shaped_mu
-    diffs_clean = np.where(shaped_X == 0, shaped_X, diffs)
-    sq_diffs = (diffs_clean*diffs_clean).sum(axis=2,keepdims=True)
-    var_not_normalized = (sq_diffs*shaped_post).sum(axis=0)
-    x_ones = np.ones((shaped_X.shape[0], shaped_X.shape[1], shaped_X.shape[2]))
-    norm_Cu = np.where(shaped_X == 0, shaped_X, x_ones).sum(axis=2, keepdims=True)
-    denominator = (shaped_post*norm_Cu).sum(axis=0)
+    mu = np.where(weights_sum < 1, mixture.mu, full_sum / weights_sum)
 
-    import pdb;pdb.set_trace()
+    nKd_mu = mu.reshape((1,K,d)).repeat(n,axis=0)
 
-    var = (var_not_normalized / denominator).reshape((var_not_normalized.shape[0]))
+    diffs = np.where(nKd_X == 0, nKd_X, nKd_X - nKd_mu)
+    sq_diffs = (diffs*diffs).sum(axis=2)
+    var_not_normalized = (sq_diffs*post).sum(axis=0)
+    x_ones = np.ones((n,K,d))
+    norm_Cu = np.where(nKd_X == 0, nKd_X, x_ones).sum(axis=2)
+    denominator = (post*norm_Cu).sum(axis=0)
+    threshold = np.ones((K)) * 0.25
+    raw_var = (var_not_normalized / denominator)
+
+    var = np.where(raw_var < 0.25, threshold, raw_var)
 
     pond = post.sum(axis=0) / post.shape[0]
 
@@ -113,4 +115,27 @@ def fill_matrix(X: np.ndarray, mixture: GaussianMixture) -> np.ndarray:
     Returns
         np.ndarray: a (n, d) array with completed data
     """
-    raise NotImplementedError
+    shaped_X = X.reshape((X.shape[0],1,X.shape[1])).repeat(mixture.mu.shape[0],axis=1)
+    shaped_mu = mixture.mu.reshape((1,mixture.mu.shape[0],mixture.mu.shape[1])).repeat(X.shape[0],axis=0)
+    shaped_var = mixture.var.reshape((1,mixture.var.shape[0],1)).repeat(X.shape[0],axis=0)
+    shaped_p = mixture.p.reshape((1,mixture.var.shape[0],1)).repeat(X.shape[0],axis=0)
+
+    shaped_var_extended = shaped_var.repeat(X.shape[1],axis=2)
+    log_N_X = -1/2*np.log(2*np.pi*shaped_var_extended)-(shaped_X-shaped_mu)**2*np.reciprocal(2*shaped_var_extended)
+    log_N_X_clean = np.where(shaped_X == 0, shaped_X, log_N_X).sum(axis=2,keepdims=True)
+    f = np.log(shaped_p) + log_N_X_clean.sum(axis=2,keepdims=True)
+    lse = logsumexp(f,axis=1,keepdims=True).repeat(f.shape[1],axis=1)
+    log_post = f-lse
+
+    post = np.exp(log_post.reshape((log_post.shape[0],log_post.shape[1])))
+
+    n, d = X.shape
+    K = mixture.mu.shape[0]
+
+    nKd_post = post.reshape(n,K,1).repeat(d,axis=2)
+    nKd_mu = mixture.mu.reshape((1,K,d)).repeat(n,axis=0)
+
+    predictions = (nKd_mu * nKd_post).sum(axis=1)
+
+    return np.where(X == 0, predictions, X)
+    
--- Unit4Project4/netflix/main.py
+++ Unit4Project4/netflix/main.py
@@ -4,10 +4,10 @@ import common
 import naive_em
 import em
 
-X = np.loadtxt("toy_data.txt")
+toy_X = np.loadtxt("toy_data.txt")
 
-def test_seeds(K):
-	print("\n############## K=" + str(K) + " ###############")
+def test_seeds(X, K):
+	print("\n############## KMEAN K=" + str(K) + " ###############")
 
 	mixture0, post0 = common.init(X,K,0)
 	mixture1, post1 = common.init(X,K,1)
@@ -40,37 +40,67 @@ def test_seeds(K):
 	print("K=" + str(K) + " seed=4 : likelihood=" + str(naive_em_estimate4[2]))
 
 
+def test_em_seeds(X, K):
+	print("\n############## EM K=" + str(K) + " ###############")
+
+	mixture0, post0 = common.init(X,K,0)
+	mixture1, post1 = common.init(X,K,1)
+	mixture2, post2 = common.init(X,K,2)
+	mixture3, post3 = common.init(X,K,3)
+	mixture4, post4 = common.init(X,K,4)
+
+	cost0 = em.run(X,mixture0,post0)[2]
+	cost1 = em.run(X,mixture1,post1)[2]
+	cost2 = em.run(X,mixture2,post2)[2]
+	cost3 = em.run(X,mixture3,post3)[2]
+	cost4 = em.run(X,mixture4,post4)[2]
+
+	print("K=" + str(K) + " seed=0 : likelihood=" + str(cost0))
+	print("K=" + str(K) + " seed=1 : likelihood=" + str(cost1))
+	print("K=" + str(K) + " seed=2 : likelihood=" + str(cost2))
+	print("K=" + str(K) + " seed=3 : likelihood=" + str(cost3))
+	print("K=" + str(K) + " seed=4 : likelihood=" + str(cost4))
+
+
 # K mean initialization
 
-test_seeds(1)
-test_seeds(2)
-test_seeds(3)
-test_seeds(4)
+test_seeds(toy_X, 1)
+test_seeds(toy_X, 2)
+test_seeds(toy_X, 3)
+test_seeds(toy_X, 4)
 
 # EM algo
 print("############## EM Algorythme implemented ###############")
-mixture, post = common.init(X,3,0)
-naive_em_estimate = naive_em.run(X,mixture,post)[2]
+mixture, post = common.init(toy_X,3,0)
+naive_em_estimate = naive_em.run(toy_X,mixture,post)[2]
 print("naive EM log likelihood : " + str(naive_em_estimate))
 
 print("############## Some Tests ######################")
-initialMixture, initialPost = common.init(X,1,0)
-mixtureEM1, postEM1, ll1 = naive_em.run(X,initialMixture,initialPost)
+initialMixture, initialPost = common.init(toy_X,1,0)
+mixtureEM1, postEM1, ll1 = naive_em.run(toy_X,initialMixture,initialPost)
 
-initialMixture, initialPost = common.init(X,2,0)
-mixtureEM2, postEM2, ll2 = naive_em.run(X,initialMixture,initialPost)
+initialMixture, initialPost = common.init(toy_X,2,0)
+mixtureEM2, postEM2, ll2 = naive_em.run(toy_X,initialMixture,initialPost)
 
-initialMixture, initialPost = common.init(X,3,0)
-mixtureEM3, postEM3, ll3 = naive_em.run(X,initialMixture,initialPost)
+initialMixture, initialPost = common.init(toy_X,3,0)
+mixtureEM3, postEM3, ll3 = naive_em.run(toy_X,initialMixture,initialPost)
 
-initialMixture, initialPost = common.init(X,4,0)
-mixtureEM4, postEM4, ll4 = naive_em.run(X,initialMixture,initialPost)
+initialMixture, initialPost = common.init(toy_X,4,0)
+mixtureEM4, postEM4, ll4 = naive_em.run(toy_X,initialMixture,initialPost)
 
-print("BIC K1 : " + str(common.bic(X, mixtureEM1, ll1)))
-print("BIC K2 : " + str(common.bic(X, mixtureEM2, ll2)))
-print("BIC K3 : " + str(common.bic(X, mixtureEM3, ll3)))
-print("BIC K4 : " + str(common.bic(X, mixtureEM4, ll4)))
+print("BIC K1 : " + str(common.bic(toy_X, mixtureEM1, ll1)))
+print("BIC K2 : " + str(common.bic(toy_X, mixtureEM2, ll2)))
+print("BIC K3 : " + str(common.bic(toy_X, mixtureEM3, ll3)))
+print("BIC K4 : " + str(common.bic(toy_X, mixtureEM4, ll4)))
 
-X_incomplete = np.loadtxt("test_incomplete.txt")
-initialMixture, initialPost = common.init(X_incomplete,4,0)
-mixtureEM4, postEM4, ll4 = em.run(X_incomplete,initialMixture,initialPost)
+X_netflix = np.loadtxt("netflix_incomplete.txt")
+test_em_seeds(X_netflix, 1)
+#test_em_seeds(X_netflix, 12)
+
+X_gold = np.loadtxt('netflix_complete.txt')
+mixture4, post4 = common.init(X_netflix,12,1)
+mixture, post, cost4 = em.run(X_netflix,mixture4,post4)
+X_pred = em.fill_matrix(X_netflix,mixture)
+
+rmse_result = common.rmse(X_gold, X_pred)
+print("RMSE between prediction and GOLD is : " + str(rmse_result))
